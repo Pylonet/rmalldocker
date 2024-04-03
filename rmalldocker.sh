36cb94bb9090a20237691e863830751ce7e04097
Pylonet <135266995+Pylonet@users.noreply.github.com>
2024-04-03 19:40:14 +0200

    Add files via upload

---
+++ rmalldocker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Colores
+rojo='\033[0;31m'
+verde='\033[0;32m'
+amarillo='\033[0;33m'
+azul='\033[0;34m'
+reset='\033[0m'
+
+echo -e "${rojo}[+]${reset}${azul} CONTENEDORES${reset}"
+
+echo -e "\n$(docker ps)\n"
+
+sleep 1.5
+
+echo -e "\n${rojo}[+]${reset}${verde} BORRANDO${reset}\n"
+echo -e "$(docker rm $(docker ps -aq ) --force)"
+
+sleep 2
+
+echo -e "\n${amarillo}[-] BORRADO CON EXITO${reset}\n"
+
+echo -e "${rojo}[+]${reset}${azul} IMAGENES${reset}"
+
+echo -e "\n$(docker images)\n"
+
+sleep 1.5
+
+echo -e "\n${rojo}[+]${reset}${verde} BORRANDO${reset}\n"
+
+echo -e "$(docker rmi $(docker images -aq ) --force)"
+
+sleep 2
+
+echo -e "\n${amarillo}[-] BORRADO CON EXITO${reset}\n"
