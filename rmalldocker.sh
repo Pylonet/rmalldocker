a45671fa167ddf59af8100747a37c39cbc271a50
Pylonet <135266995+Pylonet@users.noreply.github.com>
2024-06-24 16:22:45 +0200

    Add files via upload

--- rmalldocker.sh
+++ rmalldocker.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 
-# Colores
+# Colors
 rojo='\033[0;31m'
 verde='\033[0;32m'
 amarillo='\033[0;33m'
 azul='\033[0;34m'
 reset='\033[0m'
 
-echo -e "${rojo}[+]${reset}${azul} CONTAINERS${reset}"
+userpriv='root'
+user=$USER
 
-echo -e "\n$(docker ps)\n"
+if [ "$userpriv" == "$user" ]
+then
+  echo -e "${rojo}[!]${reset}${azul} CONTAINERS${reset}"
 
-sleep 1.5
+  echo -e "\n$(docker ps)\n"
 
-echo -e "\n${rojo}[+]${reset}${verde} DELETING${reset}\n"
-echo -e "$(docker rm $(docker ps -aq ) --force &>/dev/null)"
+  sleep 1.5
 
-sleep 2
+  echo -e "\n${rojo}[+]${reset}${verde} DELETING...${reset}\n"
+  echo -e "$(docker rm $(docker ps -aq ) --force &>/dev/null)"
 
-echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
+  sleep 2
 
-echo -e "${rojo}[+]${reset}${azul} IMAGES${reset}"
+  echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
 
-echo -e "\n$(docker images)\n"
+  echo -e "${rojo}[!]${reset}${azul} IMAGES${reset}"
 
-sleep 1.5
+  echo -e "\n$(docker images)\n"
 
-echo -e "\n${rojo}[+]${reset}${verde} DELETING${reset}\n"
+  sleep 1.5
 
-echo -e "$(docker rmi $(docker images -aq ) --force &>/dev/null)"
+  echo -e "\n${rojo}[+]${reset}${verde} DELETING...${reset}\n"
 
-sleep 2
+  echo -e "$(docker rmi $(docker images -aq ) --force &>/dev/null)"
 
-echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
+  sleep 2
 
-echo -e "${rojo}[+]${reset}${azul} VOLUMES${reset}"
+  echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
 
-echo -e "\n$(docker volume ls)\n"
+  echo -e "${rojo}[!]${reset}${azul} VOLUMES${reset}"
 
-sleep 1.5
+  echo -e "\n$(docker volume ls)\n"
 
-echo -e "\n${rojo}[+]${reset}${verde} DELETING${reset}\n"
-echo -e "\n$(docker volume rm $(docker volume ls -q) --force &>/dev/null)\n"
+  sleep 1.5
 
-sleep 2
+  echo -e "\n${rojo}[+]${reset}${verde} DELETING...${reset}\n"
+  echo -e "\n$(docker volume rm $(docker volume ls -q) --force &>/dev/null)\n"
 
-echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
+  sleep 2
+
+  echo -e "\n${amarillo}[-] SUCCESSFULLY DELETED${reset}\n"
+else
+  echo -e "\n\n${amarillo}[!]${reset}${rojo} YOU ARE NOT ROOT${reset}\n"
+fi
